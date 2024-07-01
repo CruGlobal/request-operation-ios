9ac02fb48d6034fb1838212a112a3303ab6a5b9a
Levi Eggert <levi.eggert@cru.org>
2024-07-01 10:41:37 -0400

    Exclude example project

--- Package.swift
+++ Package.swift
@@ -21,7 +21,9 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "RequestOperation",
-            dependencies: []),
+            dependencies: [],
+            exclude: ["../../Example"]
+        ),
         .testTarget(
             name: "RequestOperationTests",
             dependencies: ["RequestOperation"]),
