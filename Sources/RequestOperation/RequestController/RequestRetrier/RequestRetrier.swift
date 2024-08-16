a8dd387f6e5e0cb16ac0d88ac72ca267077d9123
Levi Eggert <levi.eggert@cru.org>
2024-08-16 15:25:26 -0400

    Add RequestController and RequestRetrier

---
+++ Sources/RequestOperation/RequestController/RequestRetrier/RequestRetrier.swift
@@ -0,0 +1,14 @@
+//
+//  RequestRetrier.swift
+//  request-operation-ios
+//
+//  Created by Levi Eggert on 8/16/2024.
+//  Copyright © 2024 Cru. All rights reserved.
+//
+
+import Foundation
+
+public protocol RequestRetrier {
+    
+    func shouldRetryRequest(response: RequestDataResponse, httpStatusCode: Int?, isSuccessHttpStatusCode: Bool) -> RetryPolicy
+}
