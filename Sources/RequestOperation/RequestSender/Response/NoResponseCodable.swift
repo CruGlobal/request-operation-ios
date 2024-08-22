66bf875d086fa381afa61da6e8d75c619c060491
Levi Eggert <levi.eggert@cru.org>
2024-08-22 14:16:48 -0400

    Add a NoResponseCodable and method to decode only success response along with method that will decode both success and failure response

---
+++ Sources/RequestOperation/RequestSender/Response/NoResponseCodable.swift
@@ -0,0 +1,13 @@
+//
+//  NoResponseCodable.swift
+//  request-operation-ios
+//
+//  Created by Levi Eggert on 8/22/2024.
+//  Copyright © 2024 Cru. All rights reserved.
+//
+
+import Foundation
+
+public struct NoResponseCodable: Codable {
+    
+}
