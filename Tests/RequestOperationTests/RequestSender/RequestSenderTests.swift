ee6ef11281001b19793f8e31c347bed84d31bdcc
Levi Eggert <levi.eggert@cru.org>
2024-09-13 10:28:12 -0400

    Add tests for URLError

--- Tests/RequestOperationTests/RequestSender/RequestSenderTests.swift
+++ Tests/RequestOperationTests/RequestSender/RequestSenderTests.swift
@@ -12,6 +12,8 @@ import Combine
 
 class RequestSenderTests: XCTestCase {
  
+    private static let englishLanguageUrl: String = "https://mobile-content-api-stage.cru.org/languages/4"
+    
     private var cancellables: Set<AnyCancellable> = Set()
     
     func testSendUrlRequestIsSuccessful() {
@@ -22,7 +24,7 @@ class RequestSenderTests: XCTestCase {
         
         let requestBuilder = RequestBuilder()
         
-        let urlString: String = "https://mobile-content-api-stage.cru.org/languages/4"
+        let urlString: String = Self.englishLanguageUrl
         let requestMethod: RequestMethod = .get
         let contentType: String = "application/vnd.api+json"
         
@@ -56,13 +58,63 @@ class RequestSenderTests: XCTestCase {
         
         wait(for: [expectation], timeout: timeoutSeconds)
         
-        let rawData: String? = responseRef?.getDataString()
-        let httpStatusCode: Int? = responseRef?.urlResponse.httpStatusCode
-        
-        print(rawData ?? "")
-        print(httpStatusCode ?? -1)
-        
         XCTAssertNotNil(responseRef)
         XCTAssertTrue(responseRef?.urlResponse.isSuccessHttpStatusCode ?? false)
+    }
+    
+    func testSendUrlRequestFailedWithUrlError() {
+        
+        let timeoutSeconds: TimeInterval = 15
+        
+        let session: URLSession = RequestUrlSession.createIgnoreCacheSession(timeoutIntervalForRequest: timeoutSeconds)
+        
+        let requestBuilder = RequestBuilder()
+        
+        let urlWithBadHost: String = "https://37469355108471600907768582811183.com/languages/4"
+        let urlString: String = urlWithBadHost
+        let requestMethod: RequestMethod = .get
+        let contentType: String = "application/vnd.api+json"
+        
+        let urlRequest: URLRequest = requestBuilder.build(
+            parameters: RequestBuilderParameters(
+                urlSession: session,
+                urlString: urlString,
+                method: requestMethod,
+                headers: ["Content-Type": contentType],
+                httpBody: nil,
+                queryItems: nil
+            )
+        )
+
+        let requestSender = RequestSender(session: session)
+        
+        let expectation = expectation(description: "")
+        
+        var errorRef: Error?
+        
+        requestSender.sendDataTaskPublisher(urlRequest: urlRequest)
+            .sink { completion in
+                
+                switch completion {
+                case .finished:
+                    break
+                    
+                case .failure(let error):
+                    errorRef = error
+                }
+                
+                expectation.fulfill()
+                
+            } receiveValue: { (response: RequestDataResponse) in
+                
+            }
+            .store(in: &cancellables)
+        
+        wait(for: [expectation], timeout: timeoutSeconds)
+        
+        XCTAssertNotNil(errorRef)
+        XCTAssertTrue(errorRef?.isUrlError ?? false)
+        XCTAssertNotNil(errorRef?.getUrlError())
+        XCTAssertTrue((errorRef as? NSError)?.domain == URLError.toErrorDomain)
     }
 }
