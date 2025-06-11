812036e786ac5a1d1fa8804fea3e63138252f2e5
Rachael Skeath <rachaelblue@gmail.com>
2025-06-11 15:33:15 -0400

    create URLSessionQueue

---
+++ Sources/RequestOperation/UrlSession/Priority/RequestPriority.swift
@@ -0,0 +1,14 @@
+//
+//  RequestPriority.swift
+//  RequestOperation
+//
+//  Created by Rachael Skeath on 6/2/25.
+//
+
+import Foundation
+
+public enum RequestPriority {
+    case low
+    case medium
+    case high
+}
